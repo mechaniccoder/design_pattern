82f20234bd92cce26de48b6c52a363288f100382
mechaniccoder <yuseunghwan94@gmail.com>
2022-09-24 19:15:43 +0900

    Observable - create it with typescript

---
+++ Observer/Observer.ts
@@ -0,0 +1,47 @@
+interface Observer {
+  update(): void;
+}
+
+function Observable() {
+  let observers: Observer[] = [];
+
+  const subscribe = (targetObserver: Observer) => {
+    observers.push(targetObserver);
+    return () => {
+      observers = observers.filter((observer) => observer !== targetObserver);
+    };
+  };
+
+  const publish = () => {
+    observers.forEach((observer) => observer.update());
+  };
+
+  return {
+    subscribe,
+    publish,
+  };
+}
+
+const observable = Observable();
+
+const unsubscribe1 = observable.subscribe({
+  update: () => console.log("observer 1"),
+});
+
+const unsubscribe2 = observable.subscribe({
+  update: () => console.log("observer 2"),
+});
+
+console.log("First publish");
+
+observable.publish();
+
+unsubscribe1();
+
+console.log("Second publish");
+observable.publish();
+
+unsubscribe2();
+
+console.log("Third publish");
+observable.publish();
