7db697ef20081861a07b8ed93932803769068332
mechaniccoder <yuseunghwan94@gmail.com>
2022-10-02 14:44:00 +0900

    Decorator: implement best practice

--- Decorator/Decorator.ts
+++ Decorator/Decorator.ts
@@ -2,7 +2,7 @@
  * If other menu is added, we should create class
  */
 abstract class Beverage {
-  constructor(private description: string) {}
+  protected description: string = "No description";
 
   getDescription() {
     return this.description;
@@ -11,22 +11,84 @@ abstract class Beverage {
   abstract cost(): number;
 }
 
-class HouseBlend extends Beverage {
-  constructor(description: string) {
-    super(description);
-  }
+abstract class CondimentDecorator extends Beverage {
+  beverage: Beverage;
+  public abstract getDescription(): string;
+}
+
+class Espresso extends Beverage {
+  protected description: string = "espresso";
 
   cost(): number {
-    return 10;
+    return 1.99;
   }
 }
 
-class DarkRost extends Beverage {
-  constructor(description: string) {
-    super(description);
-  }
+class HouseBlend extends Beverage {
+  description: string = "houseblend";
 
   cost(): number {
-    return 20;
+    return 0.89;
+  }
+}
+
+class Mocha extends CondimentDecorator {
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  public getDescription(): string {
+    return this.beverage.getDescription() + "mocha";
+  }
+
+  public cost(): number {
+    return this.beverage.cost() + 0.2;
+  }
+}
+
+class Soy extends CondimentDecorator {
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  public getDescription(): string {
+    return this.beverage.getDescription() + ",soy";
+  }
+
+  public cost() {
+    return this.beverage.cost() + 0.25;
+  }
+}
+
+class Whip extends CondimentDecorator {
+  constructor(beverage: Beverage) {
+    super();
+    this.beverage = beverage;
+  }
+
+  public getDescription(): string {
+    return this.beverage.getDescription() + ",whip";
+  }
+
+  public cost() {
+    return this.beverage.cost() + 0.3;
+  }
+}
+
+class StarbuckCoffee {
+  public static main() {
+    let beverage: Beverage = new Espresso();
+    beverage = new Mocha(beverage);
+    beverage = new Mocha(beverage);
+    beverage = new Whip(beverage);
+    console.log(beverage.cost(), beverage.getDescription());
+
+    let beverage2: Beverage = new HouseBlend();
+    beverage2 = new Soy(beverage2);
+    beverage2 = new Whip(beverage2);
+    beverage2 = new Mocha(beverage2);
+    console.log(beverage2.cost(), beverage.getDescription());
   }
 }
