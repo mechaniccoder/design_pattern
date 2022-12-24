11882f564838821d21e3c074a6e7fcec5f64cc5b
mechaniccoder <yuseunghwan94@gmail.com>
2022-12-24 15:57:02 +0900

    Adapter pattern - example

---
+++ Adapter/adapter.ts
@@ -0,0 +1,66 @@
+interface Duck {
+  quack();
+  fly();
+}
+
+class MallarDuck implements Duck {
+  quack() {
+    console.log("quack");
+  }
+  fly() {
+    console.log("flying");
+  }
+}
+
+interface Turkey {
+  gobble();
+  fly();
+}
+
+class WildTurkey implements Turkey {
+  gobble() {
+    console.log("gobble");
+  }
+  fly() {
+    console.log("flying a short distance");
+  }
+}
+
+/**
+ * Turkey adapter
+ */
+class TurkeyAdapter implements Duck {
+  constructor(private turkey: Turkey) {}
+
+  quack() {
+    this.turkey.gobble();
+  }
+
+  fly() {
+    this.turkey.fly();
+  }
+}
+
+function DuckTestDrive() {
+  const turkey: Turkey = new WildTurkey();
+  const turkeyAdapter = new TurkeyAdapter(turkey);
+
+  turkeyAdapter.quack();
+  turkeyAdapter.fly();
+}
+
+DuckTestDrive();
+
+class DuckAdapter implements Turkey {
+  constructor(private duck: Duck) {}
+
+  gobble() {
+    this.duck.quack();
+  }
+
+  fly() {
+    this.duck.fly();
+  }
+}
+
+export {};
